8677cf62010eb8f0a46f2bc88c841de53bcaf1a7
bossza <b6121822boss@gmail.com>
2025-11-07 13:29:55 +0700

    update order.model

--- models/orders.model.js
+++ models/orders.model.js
@@ -5,7 +5,7 @@ const ordersSchema = new Schema({
     user_id: { type: Schema.Types.ObjectId, ref: 'users' },
     product_id: { type: Schema.Types.ObjectId, ref: 'products' },
     price: { type: Number },
-    stock: { type: Number , default: 1 } ,
+    quantity: { type: Number , default: 1 } ,
     // approvestatus: { type: Boolean, default: false } 
 },{
     timestamps: true
